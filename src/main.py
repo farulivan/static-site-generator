1e3966da7cc6b04d775ddcaab9e25b1459ffec26
farulivan <farul@nabitu.id>
2025-12-22 14:03:35 +0700

    deploy: adjust the project for github pages deployment

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import sys
 from file_utils import copy_directory_recursive
 from page_generator import generate_pages_recursive
 
 def main():
-    copy_directory_recursive("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+    copy_directory_recursive("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -1,7 +1,7 @@
 import os
 from converters import markdown_to_html_node, extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, 'r') as f:
@@ -14,13 +14,14 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     
     full_html = template.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
+    full_html = full_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     
     with open(dest_path, 'w') as f:
         f.write(full_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for root, dirs, files in os.walk(dir_path_content):
         for file in files:
             if file.endswith('.md'):
@@ -28,4 +29,4 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 rel_path = os.path.relpath(md_path, dir_path_content)
                 html_path = rel_path.replace('.md', '.html')
                 dest_path = os.path.join(dest_dir_path, html_path)
-                generate_page(md_path, template_path, dest_path)
+                generate_page(md_path, template_path, dest_path, basepath)
