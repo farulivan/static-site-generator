767b8e8fd4b333a5a9cd416a3d6a7ff7a6489430
farulivan <farul@nabitu.id>
2025-12-22 13:50:12 +0700

    feat: add pages recursive generator

--- src/main.py
+++ src/main.py
@@ -1,13 +1,9 @@
 from file_utils import copy_directory_recursive
-from page_generator import generate_page
+from page_generator import generate_pages_recursive
 
 def main():
     copy_directory_recursive("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
-    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
-    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
-    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
-    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
--- src/page_generator.py
+++ src/page_generator.py
@@ -19,3 +19,13 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, 'w') as f:
         f.write(full_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for root, dirs, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith('.md'):
+                md_path = os.path.join(root, file)
+                rel_path = os.path.relpath(md_path, dir_path_content)
+                html_path = rel_path.replace('.md', '.html')
+                dest_path = os.path.join(dest_dir_path, html_path)
+                generate_page(md_path, template_path, dest_path)
